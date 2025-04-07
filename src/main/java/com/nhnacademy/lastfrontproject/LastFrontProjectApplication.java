d6d2ebc1b35762f28ab5f819bf1b974abd1de14a
st120712 <zzw123@naver.com>
2025-04-07 12:52:31 +0900

    first commit

---
+++ src/main/java/com/nhnacademy/lastfrontproject/LastFrontProjectApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.lastfrontproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LastFrontProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LastFrontProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nhnacademy/lastfrontproject/LastFrontProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.lastfrontproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LastFrontProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

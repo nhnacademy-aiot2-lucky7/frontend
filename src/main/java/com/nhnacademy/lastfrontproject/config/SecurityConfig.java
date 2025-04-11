3e2c32c389bf0ef7e8eb46ce1aa2f0ccb1879149
{영찬} <thddudcks12@naver.com>
2025-04-11 15:20:00 +0900

    추가- env 파일 작성 후, gitignore 등록 및 env 파일을 읽기 위한 EnvLoader 클래스 생성

--- src/main/java/com/nhnacademy/lastfrontproject/config/SecurityConfig.java
+++ src/main/java/com/nhnacademy/lastfrontproject/config/SecurityConfig.java
@@ -1,48 +1,5 @@
 package com.nhnacademy.lastfrontproject.config;
 
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-//import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
-//import org.springframework.security.web.SecurityFilterChain;
-//
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityConfig {
-//
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http
-//                .csrf((AbstractHttpConfigurer::disable))
-//                .headers(headers -> headers
-//                        // iframe으로부터의 클릭재킹 방지 (같은 도메인만 허용)
-//                        .frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)
-//
-//                        // 브라우저에게 HTTPS만 허용하라고 지시 (Strict-Transport-Security)
-//                        .httpStrictTransportSecurity(hsts -> hsts
-//                                .includeSubDomains(true)
-//                                .maxAgeInSeconds(31536000)
-//                        )
-//
-//                        // 콘텐츠 보안 정책 설정 (XSS, 외부 스크립트 방지 등)
-//                        .contentSecurityPolicy(csp -> csp
-//                                .policyDirectives("default-src 'self'; script-src 'self'; object-src 'none';")
-//                        )
-//                )
-//                .authorizeHttpRequests(auth -> auth
-//                        .requestMatchers(
-//                                "/", "/login", "/favicon.ico",
-//                                "/css/**", "/js/**", "/img/**", "/webjars/**"
-//                        ).permitAll()
-//                        .anyRequest().authenticated()
-//                );
-//
-//        return http.build();
-//    }
-//}
-
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -58,7 +15,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/", "/pages-sign-in", "/pages-sign-up",
+                        .requestMatchers("/", "/sign-in", "/sign-up",
                                 "/css/**", "/js/**", "/img/**", "/webjars/**").permitAll()
                         .anyRequest().authenticated() // 🔥 여기가 핵심
                 )
--- src/main/java/com/nhnacademy/lastfrontproject/controller/IndexController.java
+++ src/main/java/com/nhnacademy/lastfrontproject/controller/IndexController.java
@@ -8,30 +8,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
-    @GetMapping("/")
     public String index(){
         return "index";
     }
 
-    @GetMapping("/pages-sign-in")
-    public String signIn() {
-        return "pages-sign-in";
-    }
-
-//    @GetMapping("/api/member/oauth-callback/googleplus/")
-//    public String googleCallBack(@RequestParam String code, Model model) {
-//    model.addAttribute("code", code);
-
     @GetMapping("/loginSuccess")
     public String googleCallBack(@AuthenticationPrincipal OAuth2User oAuth2User, Model model){
-//        model.addAttribute("sub", oAuth2User.getAttribute("sub"));
-//        model.addAttribute("name", oAuth2User.getAttribute("name"));
-//        model.addAttribute("given_name", oAuth2User.getAttribute("given_name"));
-//        model.addAttribute("family_name", oAuth2User.getAttribute("family_name"));
-//        model.addAttribute("picture", oAuth2User.getAttribute("picture"));
-//        model.addAttribute("email", oAuth2User.getAttribute("email"));
-//        model.addAttribute("email_verified", oAuth2User.getAttribute("email_verified"));
-//        model.addAttribute("locale", oAuth2User.getAttribute("locale"));
 
         model.addAttribute("userAttributes", oAuth2User.getAttributes());
 

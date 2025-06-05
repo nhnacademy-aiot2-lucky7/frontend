84ce2641a174c973e73145eea815e4ecad80f5dd
{영찬} <thddudcks12@naver.com>
2025-06-06 01:33:00 +0900

    머지 관련 수정

--- src/main/java/com/nhnacademy/lastfrontproject/dto/analysis/AnalysisResponse.java
+++ src/main/java/com/nhnacademy/lastfrontproject/dto/analysis/AnalysisResponse.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
 import java.util.List;
 
 @Getter
@@ -12,6 +11,7 @@ import java.util.List;
 @AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class AnalysisResponse {
+
     private Result result;
 
     @Getter
@@ -19,7 +19,7 @@ public class AnalysisResponse {
     @AllArgsConstructor
     public static class Result {
         private String analysisType;
-        private List<SensorInfo> sensorInfo;
+        private List<SensorInfo> sensorInfo;  // 기존 SensorInfo 재사용
         private String model;
         private List<PredictedData> predictedData;
         private long analyzedAt;
@@ -29,18 +29,8 @@ public class AnalysisResponse {
     @NoArgsConstructor
     @AllArgsConstructor
     public static class PredictedData {
-        private SensorInfo sensorInfo;
+        private SensorInfo sensorInfo; // 기존 SensorInfo 재사용
         private double singleRiskModel;
         private double correlationRiskModel;
-    }
-
-    @Getter
-    @NoArgsConstructor
-    @AllArgsConstructor
-    public static class SensorInfo {
-        private String gatewayId;
-        private String sensorId;
-        private String sensorType;
-        private String location;
     }
 }

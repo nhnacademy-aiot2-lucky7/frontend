73f5a90de0c09a76bb11ea723b01a306923926ec
{영찬} <thddudcks12@naver.com>
2025-06-06 17:03:31 +0900

    GatewaySummaryResponse dto 객체 수정

--- src/main/java/com/nhnacademy/lastfrontproject/dto/gateway/GatewaySummaryResponse.java
+++ src/main/java/com/nhnacademy/lastfrontproject/dto/gateway/GatewaySummaryResponse.java
@@ -2,9 +2,11 @@ package com.nhnacademy.lastfrontproject.dto.gateway;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-@Getter
 @AllArgsConstructor
+@NoArgsConstructor
+@Getter
 public class GatewaySummaryResponse {
     private long gatewayId;
     private String gatewayName;
